c11a54d23e9c0d93721400eb3f96c1319fbc4bab
Pritam <www.pritamdas1999@gmail.com>
2021-07-06 02:53:38 +0530

    upload app 04

--- src/components/socket/Socket.js
+++ src/components/socket/Socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client'
 
 
-// const SERVER_URL = 'https://chat-app-api-001.herokuapp.com'
-const SERVER_URL = 'http://localhost:5000'
+const SERVER_URL = 'https://chat-app-api-001.herokuapp.com'
+// const SERVER_URL = 'http://localhost:5000'
 
 const socket = io(SERVER_URL, { transport: ['websocket'] })
 
